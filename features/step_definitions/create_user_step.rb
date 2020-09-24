c48d4923cd14cb68ade5443e5adf94084f3c8c5a
Efraim Xavier <efraimbx99@gmail.com>
2020-09-24 19:57:37 -0300

    cucumber yml escopo fixed

--- features/step_definitions/create_user_step.rb
+++ features/step_definitions/create_user_step.rb
@@ -1,5 +1,5 @@
 Quando('eu cadastro meu usuario') do
-    pending # Write code here that turns the phrase above into concrete actions
+    pending #aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
   end
   
   Então('eu verifico se o usuario foi cadastrado') do
